8f1777df38aa7c914865b1a0f2505dd8fb8e79dc
Ezra Zibel <168735774+ezibbler@users.noreply.github.com>
2024-10-21 16:58:06 -0400

    created ProgressBar check and let it display readably

--- ProgressBar.java
+++ ProgressBar.java
@@ -1,30 +1,44 @@
-class ProgressBar {
+public class ProgressBar {
     private int barLength;
-    private int startNum;
-    private int numMax;
+    private int maxValue;
     private int decimals = 1;
     private String endBuffer = "";
     private String fillChar = "#";
 
     private double lastValue = 0.0;
 
-    public ProgressBar(int barLength, int startNum, int numMax, int decimals, String endBuffer, String fillChar) {
+    public ProgressBar(int barLength, int numMax, int decimals, String endBuffer, String fillChar) {
         this.barLength = barLength;
-        this.startNum = startNum;
-        this.numMax = numMax;
+        this.maxValue = numMax;
         this.decimals = decimals;
         this.endBuffer = endBuffer;
         this.fillChar = fillChar;
     }
 
-    public String updateBar(int newNum) {
-        double barPercentage = ( Math.pow(10, 2 + decimals) * ((double) newNum / numMax) ) / Math.pow(10, 2 + decimals);
-        if (barPercentage <= lastValue) {
-            return "no change";
+    public String updateBar(int currentValue) {
+        
+        //((int) (Math.pow(10, 2 + decimals) * currentValue / maxValue)) / Math.pow(10, 2 + decimals);
+        final double BAR_PERCENTAGE = ((int) (Math.pow(10, 2 + decimals) * (currentValue / maxValue))) / Math.pow(10, 2 + decimals);
+        if (BAR_PERCENTAGE <= lastValue) {
+            return null;
         }
-        int barProportion = (int) (barPercentage * barLength);
-        lastValue = barPercentage;
-        String barString = fillChar.repeat(barProportion);
-        return endBuffer + barString + endBuffer;
+
+        try {
+            Thread.sleep(1);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        final int BAR_PROPORTION = (int) (BAR_PERCENTAGE * barLength);
+        lastValue = BAR_PERCENTAGE;
+
+        final String BAR_STRING = fillChar.repeat(BAR_PROPORTION) + " ".repeat(barLength - BAR_PROPORTION);
+        clearScreen();
+
+        return BAR_PERCENTAGE * 100 + "%" + endBuffer + "[" + BAR_STRING + "]" +endBuffer;
     }
+
+    private static void clearScreen() {  
+        System.out.print("\033[H\033[2J");  
+     } 
 }
